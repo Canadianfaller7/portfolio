6aad23023c74d72fdd77bf69a4ebaf49dcc561ae
Spencer Merrill <canadianfaller7@gmail.com>
2023-03-07 02:28:17 -0700

    got footer made and did more styling to about, footer, header, nav

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='icons'>
+      <Link to='https://github.com/Canadianfaller7' target="_blank"><AiFillGithub /></Link>
+      <Link to='https://www.linkedin.com/in/spencer-merrill-jrdevoloper777/' target="_blank"><AiFillLinkedin /></Link>
+    </footer>
   )
 }
 
