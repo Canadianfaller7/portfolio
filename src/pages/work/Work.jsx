71ca3ad6a5631a33e9d74d2b883b186bd3469ced
Spencer Merrill <canadianfaller7@gmail.com>
2023-03-07 04:13:05 -0700

    started projects page

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
+import React from 'react';
+import './work.css';
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <section id="work">
+      <h3>Projects</h3>
+      <div className="project-1">
+          <a target="_blank" href="https://canadianfaller7.github.io/beer-finder/">
+              <div className="project-info">
+                  <h4>The Beer Finder</h4>
+                  <p>jQuery / Bulma / API Integration</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-2">
+          <a target="_blank" href="https://canadianfaller7.github.io/weather-dashboard/">
+              <div className="project-info">
+                  <h4>Weather Dashboard</h4>
+                  <p>HTML | CSS | API Integration | Javascript</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-3">
+          <a target="_blank" href="https://canadianfaller7.github.io/password-generator/">
+              <div className="project-info">
+                  <h4>Random Password Generator</h4>
+                  <p>HTML | CSS | Javascript</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-4">
+          <a target="_blank" href="https://canadianfaller7.github.io/my-work-day-scheduler/">
+              <div className="project-info">
+                  <h4>Work Day Scheduler</h4>
+                  <p>HTML | CSS | Javascript</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-5">
+          <a target="_blank" href="https://canadianfaller7.github.io/javascript-code-quiz/">
+              <div className="project-info">
+                  <h4>Javascript Quiz</h4>
+                  <p>HTML | CSS | Javascript</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-6">
+          <a target="_blank" href="https://canadianfaller7.github.io/SimonSaysGame/">
+              <div className="project-info">
+                  <h4>Simon Says</h4>
+                  <p>HTML | CSS | Javascript</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-7">
+          <a target="_blank" href="https://canadianfaller7.github.io/Calculator-app/">
+              <div className="project-info">
+                  <h4>Calculator</h4>
+                  <p>HTML | CSS | Javascript</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-8">
+          <a target="_blank" href="https://canadianfaller7.github.io/Tic-Tac-Toe-App/">
+              <div className="project-info">
+                  <h4>Tic Tac Toe</h4>
+                  <p>HTML | CSS | Javascript</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-9">
+          <a target="_blank" href="https://canadianfaller7.github.io/library-of-congress-searcher/">
+              <div className="project-info">
+                  <h4>Library of Congress Search</h4>
+                  <p>HTML | CSS | Javascript</p>
+              </div>
+          </a>
+      </div>
+
+      <div className="project-10">
+          <a target="_blank" href="https://canadianfaller7.github.io/To-Do-App/">
+              <div className="project-info">
+                  <h4>To Do List</h4>
+                  <p>HTML | CSS | Javascript</p>
+              </div>
+          </a>
+      </div>
+  </section>
   )
 }
 
